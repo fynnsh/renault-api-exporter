bf1b641c60e687f184afbcd2c90dd71954bd3e6f
Fynn Würtz <fynnwuertz123466@gmail.com>
2022-03-02 22:54:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,75 @@
 import os
+import json
+import time
+import re
+import sys
+from prometheus_client import start_http_server, Gauge, Enum
 from zozo import Zoe
 
-myRenaultUser = os.getenv("RENAULT_USER")
-myRenaultPass = os.getenv("RENAULT_PASS")
+myRenaultUser = "xxxxx"
+myRenaultPass = "xxxxx"
 
-zoe = Zoe(myRenaultUser, myRenaultPass)
-zoe.getPersonnalInfo()
-# print(zoe.googleLocation())
-# zoe.batteryStatus()
-# zoe.location()
-# zoe.chargingSettings()
-# zoe.cockpit()
-# zoe.hvacStatus()
+
+# Exporter Stuff
+
+#Battery Section
+batteryLevel = Gauge('batteryLevel', 'State of Charge')
+batteryTemperature = Gauge('batteryTemperature', 'Battery Temperature')
+batteryAvailableEnergy = Gauge('batteryAvailableEnergy', 'Avail Energy in kwh')
+plugStatus = Gauge('plugStatus', 'info')
+chargingInstantaneousPower = Gauge('chargingInstantaneousPower', 'info')
+chargingStatus = Gauge('chargingStatus', 'info')
+chargingRemainingTime = Gauge('chargingRemainingTime', 'info')
+
+#Location Section
+gpsLongitude = Gauge('gpsLongitude', 'GPS LONG')
+gpsLatitude = Gauge('gpsLatitude', 'GPS LAT')
+
+#total Mileage
+totalMileage = Gauge('totalMileage', 'GPS LAT')
+
+
+if __name__ == '__main__':
+    print("Renault API exporter v0.1\n")
+    server_port = 3230
+
+    print("Running on...")
+    print("Port: " + str(server_port) + "\n")
+
+    start_http_server(server_port)
+
+
+    while True:
+
+        zoe = Zoe(myRenaultUser, myRenaultPass)
+        zoe.getPersonnalInfo()
+
+        #Battery Section
+        strin = str(zoe.batteryStatus())
+        strin = strin.replace("\'", "\"")
+        response = json.loads(strin)
+        batteryLevel.set(response['data']['attributes']['batteryLevel'])
+        batteryTemperature.set(response['data']['attributes']['batteryTemperature'])
+        batteryAvailableEnergy.set(response['data']['attributes']['batteryAvailableEnergy'])
+        plugStatus.set(response['data']['attributes']['plugStatus'])
+        chargingInstantaneousPower.set(response['data']['attributes']['chargingInstantaneousPower'])
+        chargingStatus.set(response['data']['attributes']['chargingStatus'])
+        chargingRemainingTime.set(response['data']['attributes']['chargingRemainingTime'])
+
+        #Location Section
+        strin = str(zoe.location())
+        strin = strin.replace("\'", "\"")
+        strin = strin.replace(', "gpsDirection": None', '')
+        strin = strin.replace('"gpsDirection": None,', '')
+        response = json.loads(strin)
+        gpsLongitude.set(response['data']['attributes']['gpsLongitude'])
+        gpsLatitude.set(response['data']['attributes']['gpsLatitude'])
+        
+        strin = str(zoe.cockpit())
+        strin = strin.replace("\'", "\"")
+        response = json.loads(strin)
+        totalMileage.set(response['data']['attributes']['totalMileage'])
+
+        print("Query succeed")
+
+        time.sleep(600)
